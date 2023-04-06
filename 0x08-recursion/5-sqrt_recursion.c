a40c902631bbddd781f3b4463a5f0b736567cab5
ayoubjida <59102291+ayoubjida@users.noreply.github.com>
2023-04-06 06:07:06 +0000

    Update 5-sqrt_recursion.c
    C

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,7 +8,9 @@
 
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt(0, n));
 }
 
 /**
@@ -18,13 +20,11 @@ int _sqrt_recursion(int n)
  * Return: sqrt
  */
 
-int _sqrt(int n, int i)
+int _sqrt(int n, int x)
 {
-	if (n < 0)
+	if (n > x / 2)
 		return (-1);
-	if ((i * 1) > n)
-		return (-1);
-	if (i * 1 == n)
-		return (i);
-	return (_sqrt(n, i + 1));
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
 }
